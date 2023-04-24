770551abf27ab32e14a20cdc875501fb97fb7acd
Taliane Moussassa <talianegloire392@gmail.com>
2023-04-24 14:02:51 +0100

    adds Reply class

--- Backend_TwentyOne_API/src/main/java/models/Reply.java
+++ Backend_TwentyOne_API/src/main/java/models/Reply.java
@@ -1,4 +1,43 @@
 package models;
 
 public class Reply {
+    private int gameState;
+
+    private Boolean complete;
+
+    private String message;
+
+    public Reply(int gameState, Boolean complete, String message){
+        this.gameState = gameState;
+        this.complete = complete;
+        this.message = message;
+    }
+
+    public Reply(){
+
+    }
+
+    public int getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
