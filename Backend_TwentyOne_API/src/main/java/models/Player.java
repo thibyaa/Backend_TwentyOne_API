ccb1f806280f36f5d07be1115d60b17e84c1da04
William Dorling <williamdorling@hotmail.com>
2023-04-24 12:31:30 +0100

    adds player model

--- Backend_TwentyOne_API/src/main/java/models/Player.java
+++ Backend_TwentyOne_API/src/main/java/models/Player.java
@@ -1,4 +1,54 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "players")
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @JsonIgnoreProperties({"players"})
+    @ManyToMany(mappedBy = "players")
+    private List<Game> games;
+
+    public Player(String name){
+        this.name = name;
+        this.games = new ArrayList<>();
+    }
+
+    public Player(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
 }
